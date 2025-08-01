3b92cd93e3aa2c054bb6170e885c1865480eef32
AkifKaplan <akifkpln03@gmail.com>
2025-08-01 15:38:05 +0300

    First

---
+++ _1._Hafta_1._gün.py
@@ -0,0 +1,43 @@
+print(2+2)
+print(2**3)
+print(10%3)
+
+x=1
+y=5.55
+isstudent=False
+name= "Cinar"
+print(name)
+
+x=input("1. NUMBER: ")
+y=input("2. NUMBER: ")
+
+z=int(x)+ int(y)
+print(z)
+print(type(z))
+
+z=float(z)
+print(z)
+print(type(z))
+
+isOnline = True
+
+isOnline = str(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+#Given a radius of circle. Find the perimeter and area.
+pi = 3.14 
+
+print("Please enter radius of circle")
+r = input("Please enter radius of circle: ")
+r=int(r)
+print(r)
+print(type(r))
+
+#area
+z=r*r*pi
+print("Area:" ,z)
+
+#perimeter
+p=2*pi*r
+print("Perimeter:",p)
